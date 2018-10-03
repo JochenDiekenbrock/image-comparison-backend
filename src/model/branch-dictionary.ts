2d444ebb2613212b125823b85dde1b4d480ac7e2
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-10-03 23:52:14 +0200

    documentation

--- src/model/branch-dictionary.ts
+++ src/model/branch-dictionary.ts
@@ -1,3 +1,7 @@
+/*
+    maps the normalized branch name (for example example-features-feature1)
+    to the branch directory (for example example-features/feature1)
+*/
 export interface BranchDictionary {
     [branch: string]: string;
 }
